a70fd7bd6a3c2c9f0e27f8b05a8aac0bebad249d
jfcslc801 <jfcslc@outlook.com>
2025-06-28 02:59:41 -0400

    firebase set up, login with phone number, admin login takes user to admin view

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBAGZ2tMj3AKjiLtOvaDHPW4XMYpdTR2VQ",
+  authDomain: "mi-casita-7ca95.firebaseapp.com",
+  projectId: "mi-casita-7ca95",
+  storageBucket: "mi-casita-7ca95.firebasestorage.app",
+  messagingSenderId: "770244630610",
+  appId: "1:770244630610:web:66122916ee6de23efa278b",
+  measurementId: "G-T37B4SMQEZ"
+};
+
+export const app = initializeApp(firebaseConfig);
